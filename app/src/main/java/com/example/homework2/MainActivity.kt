98d8d9041c96a6e8a98386ffe34705ed641fcd97
ajatusvirhe <164178363+ajatusvirhe@users.noreply.github.com>
2026-01-26 17:14:24 +0200

    hw1 copied

--- app/src/main/java/com/example/homework2/MainActivity.kt
+++ app/src/main/java/com/example/homework2/MainActivity.kt
@@ -4,14 +4,48 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+//import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.homework2.ui.theme.Homework2Theme
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Surface
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+
+import androidx.compose.foundation.border
+import androidx.compose.material3.MaterialTheme
+//import androidx.compose.material3.Surface
+import android.content.res.Configuration
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+//import androidx.compose.runtime.getValue
+//import androidx.compose.runtime.setValue
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+//import com.example.homework2.ui.theme.Homework2Theme
+
+// most of the code is copied from the tutorial
+// https://developer.android.com/jetpack/compose/tutorial
+// with some changes to variable names
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,30 +53,91 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Homework2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface {
+                    Conversation(SampleData.conversationSample)
+                    //MessageCard(Message("Android", "Jetpack Compose"))
                 }
             }
         }
     }
 }
 
-// commit test
+data class Message(val author: String, val body: String)
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Conversation(messagelist: List<Message>) {
+    LazyColumn {
+        items(messagelist) { message ->
+            MessageCard(message)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
+fun PreviewConversation() {
     Homework2Theme {
-        Greeting("Android")
+        Conversation(SampleData.conversationSample)
+    }
+}
+
+@Composable
+fun MessageCard(msg:Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = if(msg.author == "Kethu") painterResource(R.drawable.kethucrop)
+            else painterResource(R.drawable.napakettu_crop),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp) // Set image size to 40 dp
+                .clip(CircleShape) // Clip image to be shaped as a circle
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+        )
+
+        Column( Modifier.clickable { isExpanded = !isExpanded }) {
+            Text( // author
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            //message body
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier.animateContentSize().padding(1.dp)) {
+                Text(text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium)
+            }
+        }
+    }
+}
+
+//@Preview(showBackground = true)
+@Preview(name = "Light Mode", showBackground = true)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun PreviewMessageCard() {
+    Homework2Theme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            MessageCard (
+                msg = Message("Kethu", "Aasi svengaa")
+            )
+        }
     }
 }
---
+++ app/src/main/java/com/example/homework2/SampleData.kt
@@ -0,0 +1,78 @@
+import com.example.homework2.Message
+
+/**
+ * SampleData for Jetpack Compose Tutorial 
+ */
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Kethu",
+            "Mäy...Mäy...Mäy..."
+        ),
+        Message(
+            "Napakettu",
+            """List of Android versions:
+            |Android KitKat (API 19)
+            |Android Lollipop (API 21)
+            |Android Marshmallow (API 23)
+            |Android Nougat (API 24)
+            |Android Oreo (API 26)
+            |Android Pie (API 28)
+            |Android 10 (API 29)
+            |Android 11 (API 30)
+            |Android 12 (API 31)""".trim()
+        ),
+        Message(
+            "Kethu",
+            """We are both in the same boat ... and it's called Titanic.""".trim()
+        ),
+        Message(
+            "Napakettu",
+            "Searching for alternatives to XML layouts..."
+        ),
+        Message(
+            "Napakettu",
+            """Hey, take a look at Jetpack Compose, it's great!
+            |It's the Android's modern toolkit for building native UI.
+            |It simplifies and accelerates UI development on Android.
+            |Less code, powerful tools, and intuitive Kotlin APIs :)""".trim()
+        ),
+        Message(
+            "Kethu",
+            "Aasi svengaa :)"
+        ),
+        Message(
+            "Napakettu",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        Message(
+            "Napakettu",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        Message(
+            "Napakettu",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        Message(
+            "Kethu",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        Message(
+            "Napakettu",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        Message(
+            "Napakettu",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            "Napakettu",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+        Message(
+            "Kethu",
+            "Mäy mäy mäy"
+        ),
+    )
+}
